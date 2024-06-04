5f6e436877e77fca168990b28d325101e3865f8d
Weyder <weyderfs@gmail.com>
2024-06-04 14:35:51 -0300

    First commit - including S3 tricks

---
+++ s3-bucket/README.md
@@ -0,0 +1,6 @@
+# Summary of the commands os script files
+---
+
+## Check if a S3 is Public or Private
+
+ - [check-bucket.sh](check-bucket.sh) - Check the script maybe it's necessary remove `--profile` if you not use it.
---
+++ s3-bucket/check-bucket.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Get the list of all buckets
+buckets=$(aws s3api list-buckets --profile prd | jq -r ".Buckets[].Name")
+
+for bucket in $buckets; do
+    echo "Checking bucket: $bucket"
+
+    # Check bucket ACL for public access
+    acl=$(aws s3api get-bucket-acl --bucket $bucket --profile prd)
+    if echo $acl | grep -q 'AllUsers'; then
+        echo "Bucket $bucket has public access through ACL"
+        continue
+    fi
+
+    # Check bucket policy for public access
+    policy_status=$(aws s3api get-bucket-policy-status --bucket $bucket --profile prd)
+    if echo $policy_status | grep -q '"IsPublic": true'; then
+        echo "Bucket $bucket has public access through policy"
+        continue
+    fi
+
+    echo "Bucket $bucket is not public"
+done
